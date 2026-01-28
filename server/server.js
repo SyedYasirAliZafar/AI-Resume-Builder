c3594ebf3a41ed861d75d1ee97bb400f5ef6f206
SyedYasirAliZafar <yasirali1234zafar@gmail.com>
2026-01-28 15:14:06 +0500

    User Model, Controllers and Routes created

--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
 import express from 'express'
 import cors from 'cors'
 import "dotenv/config"
+import connectDB from './config/db.js'
+import userRouter from './routes/userRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+// Database Connection
+
+await connectDB()
 
 app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Server is Live')
+  res.send('Server is Live...')
 })
+
+app.use('/api/users', userRouter)
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
